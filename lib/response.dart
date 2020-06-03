11a2d2e12f97f9e38df6c48b62eed2fe40e364b4
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2020-06-03 16:58:59 +0300

    ISO-8859-9 decoding utility applied

--- lib/response.dart
+++ lib/response.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart';
+import 'iso-8859-9.dart';
 
 /// Response extension extends the functionality of the [Response] type by
 /// adding a couple of useful methods.
@@ -12,11 +13,14 @@ import 'package:http/http.dart';
 /// Adds [asList] method to cast the JSON decoded body as a [List] of the given
 /// [Type] by applying the given transformation function to each item.
 extension ResponseExtension on Response {
+  /// Get the Latin-5 decoded body.
+  String get _body => decodeISO88599(bodyBytes);
+
   /// JSON decoded [body] of the request, as-is.
   ///
   /// Returns a dynamic object that might be a `Map<String, dynamic>` or
   /// `List<Map<String, dynamic>>` on runtime according to decoded [body].
-  get jsonBody => json.decode(body);
+  get jsonBody => json.decode(_body);
 
   /// Cast the [jsonBody] as [Type] by applying the transformation function.
   T as<T>(T f(Map<String, dynamic> e)) => f(jsonBody);
