d4b1bd49eea7e6cd8ca5c3fca38b7f15811b7c02
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2020-06-15 13:29:47 +0300

    ISO-8859-9 example created

---
+++ example/iso-8859-9.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:asis/asis.dart';
+
+var token = 'TU4pa9y8jtx3fsuMlk9e598Kcfh0ihB10Go78D6zwN-'
+    'b5EW0FDZGug085Wq5CowuJ596r6zLkHKCkWN6q13GM582h6426e'
+    '0CSidKMCEyOychW0iC6mN0MtBAh0DHUH6U36s42a8aknk0p98Js'
+    '4rbYLRI8qc';
+
+Future<dynamic> get request => http.get(
+      'https://reformalican.com/admin/oauth/identity',
+      headers: {'Authorization': 'Bearer $token'},
+    ).jsonBody;
+
+void main() async {
+  /// Override bad-certificate callback.
+  HttpOverrides.global = CertificateHttpOverride();
+
+  /// Print decoded json map.
+  request.then(print);
+}
+
+class CertificateHttpOverride extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (_, __, ___) => true;
+  }
+}
