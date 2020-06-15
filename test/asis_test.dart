fb001e7dc60d09d594cfc607442ab6d3757011f1
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2020-06-15 13:15:47 +0300

    Future<Response> test created

--- test/asis_test.dart
+++ test/asis_test.dart
@@ -3,6 +3,7 @@ import 'list_test.dart' as list_test;
 import 'response_test.dart' as response_test;
 import 'future-iterable_test.dart' as future_iterable_test;
 import 'future-list_test.dart' as future_list_test;
+import 'future-response_test.dart' as future_response_test;
 
 void main() {
   iterable_test.main();
@@ -10,4 +11,5 @@ void main() {
   response_test.main();
   future_iterable_test.main();
   future_list_test.main();
+  future_response_test.main();
 }
---
+++ test/future-response_test.dart
@@ -0,0 +1,81 @@
+import 'package:test/test.dart';
+import 'package:asis/asis.dart';
+import 'etc/index.dart';
+
+void main() {
+  group('Future<Response> transformation', () {
+    group('On single result', () {
+      test('jsonBody from response', () async {
+        expect(await singleResponseFuture.jsonBody, single);
+      });
+
+      test('Strict transforming', () async {
+        expect(await singleResponseFuture.as<bool>((e) => e['boolean']), true);
+      });
+
+      test('Dynamic transforming', () async {
+        expect(await singleResponseFuture.as((e) => e['boolean']), true);
+      });
+
+      test('Strict transforming to CustomObject', () async {
+        expect(
+          await singleResponseFuture
+              .as<CustomObject>((e) => CustomObject.fromJson(e)),
+          CustomObject.fromJson(single),
+        );
+      });
+
+      test('Dynamic transforming to CustomObject', () async {
+        expect(
+          await singleResponseFuture.as((e) => CustomObject.fromJson(e)),
+          CustomObject.fromJson(single),
+        );
+      });
+    });
+
+    group('On multiple result', () {
+      test('jsonBody from response', () async {
+        expect(await multipleResponseFuture.jsonBody, multiple);
+      });
+
+      test('Strict transforming', () async {
+        expect(
+          await multipleResponseFuture.asList<bool>((e) => e['boolean']),
+          [true, true],
+        );
+      });
+
+      test('Dynamic transforming', () async {
+        expect(
+          await multipleResponseFuture.asList((e) => e['boolean']),
+          [true, true],
+        );
+      });
+
+      test('Strict transforming to CustomObject', () async {
+        expect(
+          await multipleResponseFuture.asList<CustomObject>(
+            (e) => CustomObject.fromJson(e),
+          ),
+          multiple.map((e) => CustomObject.fromJson(e)),
+        );
+      });
+
+      test('Dynamic transforming to CustomObject', () async {
+        expect(
+          await multipleResponseFuture.asList((e) => CustomObject.fromJson(e)),
+          multiple.map((e) => CustomObject.fromJson(e)),
+        );
+      });
+
+      test('Future<List<T>> manipulation', () async {
+        expect(
+          await multipleResponseFuture
+              .asIterable((e) => CustomObject.fromJson(e))
+              .asList<bool>((e) => e.boolean),
+          multiple.as((e) => CustomObject.fromJson(e).boolean),
+        );
+      });
+    });
+  });
+}
